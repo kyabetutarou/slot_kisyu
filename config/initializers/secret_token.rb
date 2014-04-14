1b33a4a5181245b42d5f8c7a739b3ab5d8c2f19f
kyabetutarou <naokazu777@gmail.com>
2014-04-14 12:23:02 +0900

    初期インストール

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlotKisyu::Application.config.secret_key_base = '9ae4bd86a8ad62a151e657993e218f506933dbd73d11a34844be3df5abf9c5720c741266caea20eceba207ef8c137a525d943e6a723a6f81fdb34c8973abef6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlotKisyu::Application.config.session_store :cookie_store, key: '_slot_kisyu_session'
